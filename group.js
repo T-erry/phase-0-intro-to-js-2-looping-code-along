01f4f0110db4e5a9b6fe4bf02e2e88946f7d1e64
T-erry <terrywachuka10@gmail.com>
2022-11-19 14:09:42 +0300

    completed assignment

---
+++ group.js
@@ -0,0 +1,23 @@
+// let myArray = ['a', 'b', 'c', 'd', 'e', 'f', 'g'];
+
+// for (let i = 0; i < myArray.length; i++) {
+//   console.log(myArray[i]);
+// }
+
+// while (let age = 30; age < 40; age++) {
+//     console.log(`I'm ${age} years old. Happy birthday to me!`);
+//     debugger;
+//   }
+
+let age = 30;
+while ( age < 40) {
+    console.log(`I'm ${age++} years old. Happy birthday to me!`);
+    debugger;
+  }
+// for (let countup = 0; countup < 10; countup++) {
+//     console.log(countup);
+//   }
+//   let countup = 0;
+//   while (countup < 10) {
+//     console.log(countup++);
+//   }
--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Code your solutions in this file
+let name = [];
+
+function writeCards(name, suprise) {
+    for (let i = 0; i < name.length; i++) {
+        name[i] = `Thank you, ${name[i]}, for the wonderful ${suprise} gift!`;
+    }
+    return name;
+}
+
+writeCards(["Guadalupe", "Ollie", "Aki"], "surprise")
+
+// let number = 0;
+
+// function countDown(number){
+//     while(number < 10 ){
+//         console.log(number++);
+//     }
+// }
+
+
+function countDown(number) {
+    while (number >= 0) {
+        console.log(number--);
+        debugger;
+    }
+}
+    countDown(4)
+
